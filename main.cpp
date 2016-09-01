4e2eb55670019d6958f6d4856463dd0f27260c7c
Charles Clayton <chaeclay@ut.utm.edu>
2016-08-31 19:11:26 -0500

    Updated main.cpp to asker user for input and show the sum from 1 to the input.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,26 @@
-
+//CHARLES CLAYTON
 #include <iostream>
 
 using namespace std; 
 
+int sum(int n){
+  int total = 0;
+  for(int i = 0; i < n; i++){
+    total += i;
+  }
+  return total;
+}
+
 int main() {
 
   cout << "Hello World!" << endl;
+  
+  cout << "Enter an integer: ";
+  int n = 0;
+  cin >> n;
 
+  cout << "The sum from 1 to " << n << " is: " << sum(n) << endl;
+  
+  
 return 0; 
 }
